4ec8fc4cf0a0d77367393f9618badf6ab16d6c5e
Sophia X <131772407+sophiaxu-nhm@users.noreply.github.com>
2024-10-29 15:48:29 +0000

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  /**
+   * @param {import('webpack').Configuration} config
+   */
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
-    })
-    return config
-  }
-}
+    });
+    return config;
+  },
+};
